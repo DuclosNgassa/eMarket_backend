53a0201dc7f9c6e4ad2eb49a18d641baa9c88776
Duclos Ngassa <armel-duclos.ndanji-ngassa@adesso.de>
2019-08-12 11:14:56 +0200

    models

--- routes/images.js
+++ routes/images.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const Image = require('../models/Image')
+const Image = require('../models/Image');
+const Post = require('../models/Post');
+
 //Insert
 router.post('/', async (req, res, next) => {
     res.send({
