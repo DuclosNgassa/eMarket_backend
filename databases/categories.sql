fdccc400f50006f3d3e94dd4331e0fd794bd7e1d
Duclos Ngassa <armel-duclos.ndanji-ngassa@adesso.de>
2019-11-14 23:50:52 +0100

    script for categorie

---
+++ databases/categories.sql
@@ -0,0 +1,113 @@
+INSERT INTO categories (title,parentid,icon) VALUES
+('Voiture, Moto & Camion',NULL,'0xe531')
+,('Vélos & Accessoires',1,NULL)
+,('Motos & Accessoires',1,NULL)
+,('Camions & Accessoires',1,NULL)
+,('Mecanicien',1,NULL)
+,('Autres Voitures, Moto & Camion',1,NULL)
+,('Autres',NULL,'0xe5d3')
+,('Décoration',9,NULL)
+,('Voitures',1,NULL)
+,('Pieces de voiture et pneu',1,NULL)
+;
+INSERT INTO categories (title,parentid,icon) VALUES
+('Restaurant & Service traiteur',NULL,'0xe56c')
+,('Jobs',NULL,'0xe8f9')
+,('Immobilier',NULL,'0xe88a')
+,('Service traiteur',21,NULL)
+,('Mecanicien',21,NULL)
+,('Depannage',21,NULL)
+,('Decoration d´évenement',21,NULL)
+,('Autres prestations de service ',21,NULL)
+,('Téléphone',11,NULL)
+,('Électromenager',11,NULL)
+;
+INSERT INTO categories (title,parentid,icon) VALUES
+('Laptops & PCs',11,NULL)
+,('Logiciel & Accessoires d´ordinateur',11,NULL)
+,('Tablets',11,NULL)
+,('TV',11,NULL)
+,('Jeux vidéos',11,NULL)
+,('Dépannage électronique',11,NULL)
+,('Domestique & Babysitting',18,NULL)
+,('Habits & Chaussures de bébés',18,NULL)
+,('Habits & Chaussures d´enfants',18,NULL)
+,('Accessoires de Bébés',18,NULL)
+;
+INSERT INTO categories (title,parentid,icon) VALUES
+('Autres Famille, Enfant & Bébé',18,NULL)
+,('Famille, Enfants & Bébé',NULL,'0xeb42')
+,('Restaurant',5,NULL)
+,('Service traiteur',5,NULL)
+,('Jeux pour Bébé',18,NULL)
+,('Maison à louer',24,NULL)
+,('Maison à vendre',24,NULL)
+,('Terrain à vendre',24,NULL)
+,('Service de déménagement',24,NULL)
+,('Autres immobilier',24,NULL)
+;
+INSERT INTO categories (title,parentid,icon) VALUES
+('Menage',27,NULL)
+,('Transport',27,NULL)
+,('Gastronomie & Tourisme',27,NULL)
+,('Construction',27,NULL)
+,('Vente',27,NULL)
+,('Stage de vacances',27,NULL)
+,('Santé',27,NULL)
+,('Autres jobs',27,NULL)
+,('Don',29,NULL)
+,('Échange',29,NULL)
+;
+INSERT INTO categories (title,parentid,icon) VALUES
+('Location',29,NULL)
+,('Roman',31,NULL)
+,('Materiel scolaire',31,NULL)
+,('Livres scolaires & academiques',31,NULL)
+,('Autres Livres & Film',31,NULL)
+,('Concert',9,NULL)
+,('Mariage',9,NULL)
+,('Anniversaire',9,NULL)
+,('Autres Évènementiel',9,NULL)
+,('Art & Musique',13,NULL)
+;
+INSERT INTO categories (title,parentid,icon) VALUES
+('Bricollage',13,NULL)
+,('Objects pour salle de bain',22,NULL)
+,('Meubles de bureau',22,NULL)
+,('Meubles de salon',22,NULL)
+,('Meubles de chambre',22,NULL)
+,('Decoration',22,NULL)
+,('Lampe & éclairage',22,NULL)
+,('Sport & Fitness',13,NULL)
+,('Livres & Film',NULL,'0xe431')
+,('Évènementiel',NULL,'0xe540');
+
+INSERT INTO categories (title,parentid,icon) VALUES
+('Santé & Beauté',28,NULL)
+,('Restauration & Patisserie',28,NULL)
+,('Cours d´Informatik',28,NULL)
+,('Cours de musique',28,NULL)
+,('Cours de danse',28,NULL)
+,('Menuiserie',28,NULL)
+,('Cours de langue',28,NULL)
+,('Cours de répétition',28,NULL)
+,('Cours d´Art',28,NULL)
+,('Bijoux & Accessoires',30,NULL);
+
+INSERT INTO categories (title,parentid,icon) VALUES
+('Santé & Beauté',30,NULL)
+,('Vêtements de femmes',30,NULL)
+,('Chaussures de femmes',30,NULL)
+,('Vêtements d´hommes',30,NULL)
+,('Engins & Accessoires',NULL,'0xe558')
+,('Jeux & Loisirs',NULL,'0xeb48')
+,('Chaussures d´hommes',30,NULL)
+,('Autres Mode & Beauté',30,NULL)
+,('Don & Echanges',NULL,'0xe8f6')
+,('Electronique',NULL,'0xe325');
+
+INSERT INTO categories (title,parentid,icon) VALUES
+('Prestation de service',NULL,'0xe8f9')
+,('Cours & Formation',NULL,'0xe80c')
+,('Maison & Gardin',NULL,'0xe16b')
+,('Mode & Beauté',NULL,'0xe63d');
