44470e60315edfaa91f7bf70680ebfd9fa731dea
Duclos Ngassa <armel-duclos.ndanji-ngassa@adesso.de>
2019-11-30 05:42:30 +0100

    - db connection

--- databases/database.js
+++ databases/database.js
@@ -1,19 +1,23 @@
 const Sequelize = require('sequelize');
 
-var sequelize = null;
-
-if (process.env.DATABASE_URL) {
-    // the application is executed on Heroku ... use the postgres database
-    //sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres',
-        protocol: 'postgres',
-        //port: match[4],
-        //host: match[3],
-        logging: true //false
-    });
-} else {
-    sequelize = new Sequelize(
+const sequelize = new Sequelize(
+        'postgres', // db name
+        'postgres', // username
+        'Ichbin1postgres',
+        {
+            dialect: 'postgres',
+            schema: 'emarket',
+            host: 'https//144.91.105.5',
+            pool: {
+                max: 5,
+                min: 0,
+                require: 30000,
+                idle: 10000
+            }
+        }
+    );
+/*
+const sequelize = new Sequelize(
         'postgres', // db name
         'armelduclosndanjingassa', // username
         '123456',
@@ -29,7 +33,7 @@ if (process.env.DATABASE_URL) {
             }
         }
     );
-}
+*/
 
 const Op = Sequelize.Op;
 module.exports = {
