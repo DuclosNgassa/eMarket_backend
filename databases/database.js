14b617b5d55fe717cdb7470607cc8ebb756aab8c
Duclos Ngassa <armel-duclos.ndanji-ngassa@adesso.de>
2019-12-01 23:42:48 +0100

    - db connection

--- databases/database.js
+++ databases/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 
 const sequelize = new Sequelize(
-        'postgres', // db name
+        'emarket', // db name
         'postgres', // username
         'Ichbin1postgres',
         {
