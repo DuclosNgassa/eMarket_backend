e37072b9d8d04c947bda0fb4f365fa6d1c3c5b6c
Duclos Ngassa <armel-duclos.ndanji-ngassa@adesso.de>
2020-02-20 06:02:34 +0100

    bugfix in image

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
